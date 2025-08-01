eaf3291e185b93f179ec81fef7c6a117379e8254
dinosOnVenus <ozlemgoksun1389@gmail.com>
2025-08-01 17:42:58 +0300

    NewAttempt

---
+++ LeetCode in Csharp/01_TwoSum_(Easy)/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_TwoSum__Easy_
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Solution solution = new Solution();
+
+                int[] nums;
+                int target, num;
+
+                Console.Write("Enter the length of the array:");
+                num = int.Parse(Console.ReadLine());
+
+                nums = new int[num];
+
+                for (int i = 0; i < num; i++)
+                {
+                    Console.Write(i + ". Element : ");
+                    nums[i] = int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("Enter the target num: ");
+                target = int.Parse(Console.ReadLine());
+
+                int[] result = solution.TwoSum(nums, target);
+
+                if (result.Length > 0)
+                {
+                    Console.WriteLine($"Result: [{result[0]}, {result[1]}]");
+                }
+                else
+                {
+                    Console.WriteLine("Couldn't found a match.");
+                }
+            }
+
+            public class Solution
+            {
+                public int[] TwoSum(int[] nums, int target)
+                {
+                    for (int i = 0; i < nums.Count() - 1; i++)
+                    {
+                        for (int j = i + 1; j < nums.Count(); j++)
+                        {
+                            if (nums[i] + nums[j] == target)
+                            {
+                                return new int[2] { i, j };
+                            }
+                        }
+                    }
+                    return new int[0];
+                }
+            }
+        }
+    }
---
+++ LeetCode in Csharp/09_PalindromeNumber_(Easy)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_PalindromeNumber__Easy_
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Solution solution = new Solution();
+
+                int x;
+
+                Console.Write("Enter a number : ");
+                x = int.Parse(Console.ReadLine());
+
+            }
+
+            public class Solution
+            {
+                public bool IsPalindrome(int x)
+                {
+
+                    if (x % 10 == ((x - (x % 100)) / 100) && x > 0)
+                    {
+                        return true;
+                    }
+                    else if ((x - x % 10) - ((x % 10) * 10) == 0 && x > 0)
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+            }
+        }
+    }
+

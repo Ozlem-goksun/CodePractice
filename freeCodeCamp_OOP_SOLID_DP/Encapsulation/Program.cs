f7e5c423f9d76a3d8b28acfb6c59fe03a80b9165
dinosOnVenus <ozlemgoksun1389@gmail.com>
2025-08-06 16:08:56 +0300

    Encapsulation

---
+++ freeCodeCamp_OOP_SOLID_DP/Encapsulation/Program.cs
@@ -0,0 +1,12 @@
+﻿//Encapsulations
+
+using Encapsulation;
+
+BankAccount bankAccount = new BankAccount(200);
+System.Console.WriteLine(bankAccount.GetBalance());
+
+bankAccount.Deposit(50);
+System.Console.WriteLine(bankAccount.GetBalance());
+
+bankAccount.Withdraw(100);
+System.Console.WriteLine(bankAccount.GetBalance());

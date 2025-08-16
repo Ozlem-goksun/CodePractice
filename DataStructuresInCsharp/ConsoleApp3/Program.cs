4ef0f671ab9e80ec5d16d51348211691af936540
dinosOnVenus <ozlemgoksun1389@gmail.com>
2025-08-16 21:35:13 +0300

    class

---
+++ DataStructuresInCsharp/ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher(8645, "Jack","Paul",true);
+
+            Console.WriteLine(teacher);
+        }
+    }
+}
---
+++ DataStructuresInCsharp/ConsoleApp3/Teacher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Teacher
+    {
+        public int Num { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool Gender { get; set; }
+
+        //Default ctor
+       public Teacher()
+        {
+
+        }
+
+        public Teacher(int num, string name, string surname, bool gender)
+        {
+            Num = num;
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"{Num} {Name} {Surname} " +
+                string.Format(Gender == true ? "Woman" : "Man");
+        }
+    }
+}

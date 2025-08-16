81793bd4bcd2bf823c597958d38995555318ed0d
dinosOnVenus <ozlemgoksun1389@gmail.com>
2025-08-16 20:52:50 +0300

    value types

---
+++ DataStructuresInCsharp/ConsoleApp2/Point.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp2
+{
+    public struct Point
+    {
+        public int XPoint { get; set; }
+        public int YPoint { get; set; }
+
+        public override string ToString()
+        {
+            return $"{XPoint},{YPoint}";
+        }
+
+        // Default ctor can't uses directly in structs
+        public Point(int x, int y)
+        {
+            XPoint = x;
+            YPoint = y;
+        }
+
+        public void SetOrigin()
+        {
+            this.XPoint = 0;
+            this.YPoint = 0;
+        }
+
+        public void Change()
+        {
+            var temp = this.XPoint;
+            XPoint = this.YPoint;
+            YPoint = temp;
+        }
+
+    }
+}
---
+++ DataStructuresInCsharp/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var point1 = new Point(56, 96);
+            Console.WriteLine(point1);
+            point1.Change();
+            Console.WriteLine(point1);
+            var point2 = point1;
+            Console.WriteLine(point2);
+
+            point2.XPoint = -1 * point2.XPoint;
+            Console.WriteLine(point1);
+            Console.WriteLine(point2);
+
+        }
+    }
+}

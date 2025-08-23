13bee312f4b342474145f3894172d20f7c2b71df
dinosOnVenus <ozlemgoksun1389@gmail.com>
2025-08-23 18:09:43 +0300

    sortedList

---
+++ DataStructuresInCsharp/ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var icindekiler = new SortedList()
+            {
+                {5, "Bolum bir"},
+                {23, "Bolum iki"},
+                {58, "Bolum dort"},
+                {89, "Bolum bes"}
+
+            };
+
+            Console.WriteLine("icindekiler");
+            Console.WriteLine(new string('=',23));
+            foreach ( DictionaryEntry i in icindekiler)
+            {
+                Console.WriteLine($"{i.Key, -5}{i.Value}");
+            }
+        }
+    }
+}

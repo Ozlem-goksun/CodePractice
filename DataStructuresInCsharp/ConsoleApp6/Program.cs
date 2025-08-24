cd36d980422747c7853fec721091f79f1f00cac5
dinosOnVenus <ozlemgoksun1389@gmail.com>
2025-08-24 10:58:04 +0300

    queue

---
+++ DataStructuresInCsharp/ConsoleApp6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var charQueue = new Queue<char>();
+
+            charQueue.Enqueue('a');
+            charQueue.Enqueue('e');
+
+            foreach (char c in charQueue)
+            { 
+                Console.WriteLine(c); 
+            }
+
+            charQueue.Dequeue();
+
+            foreach (var item in charQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
